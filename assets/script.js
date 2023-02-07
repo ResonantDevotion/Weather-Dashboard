0b5974f684aba773529a0516698f1d4646080314
rebecca wood <rebeccaseverowood@gmail.com>
2023-02-07 20:17:50 +1300

    Added the local storage which will set the city, need to add get and the history buttons.

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ $("#search-button").on("click", function (event) {
     let city = $('#search-input').val().trim();
     let geoQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
 
-    $.ajax({
+    let cityAjax = $.ajax({
         url: geoQueryURL,
         method: "GET"
     }).then(function (response) {
@@ -29,7 +29,7 @@ $("#search-button").on("click", function (event) {
             console.log(result);
             const forecast = result.list;
             let forecastArray = []
-            for (let i = 2; i < forecast.length; i += 8){
+            for (let i = 2; i < forecast.length; i += 8) {
                 forecastArray.push(forecast[i]);
 
             };
@@ -55,29 +55,35 @@ $("#search-button").on("click", function (event) {
 
     // Function to display CURRENT city name, date, temp, humidity, wind speed data related to city + related icon
     function displayWeatherData(result) {
+        //Getting the current city to diplay on html
         let currentCity = $('<h3>');
         currentCity.text("City: " + result.name)
         console.log(result.name);
 
+        //Adding the icon
         let icon = $('<img>');
         icon.attr("src", `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`);
 
+        //adding the icon and city to a header on the html
         let header = $('<div>');
         header.addClass('d-flex justify-content-between');
         header.append(currentCity);
         header.append(icon);
         $('#today').append(header);
 
+        //Getting the current temp to diplay on html
         let currentTemp = $('<h3>');
         currentTemp.text("Temperature: " + result.main.temp)
         console.log(result.main.temp);
         $('#today').append(currentTemp);
 
+        //Getting the current humidity to diplay on html
         let currentHum = $('<h3>');
         currentHum.text("Humidity: " + result.main.humidity + "%")
         console.log(result.main.humidity);
         $('#today').append(currentHum);
 
+        //Getting the current wind speed to diplay on html
         let currentWind = $('<h3>');
         currentWind.text("Wind speed: " + result.wind.speed + "kph")
         console.log(result.wind.speed);
@@ -93,6 +99,17 @@ $("#search-button").on("click", function (event) {
         $('#forecast').append(nextDay);
         console.log(forecastArray.dt);
 
+        //Adding the icon
+        let icon = $('<img>');
+        icon.attr("src", `http://openweathermap.org/img/wn/${forecastArray.weather[0].icon}@2x.png`);
+
+        //adding the icon and date to a header on the html
+        let header = $('<div>');
+        header.addClass('d-flex justify-content-between');
+        header.append(nextDay);
+        header.append(icon);
+        $('#forecast').append(header);
+
         //temp
         let nextDayTemp = $('<h3>');
         nextDayTemp.text("Temperature: " + forecastArray.main.temp)
@@ -106,13 +123,14 @@ $("#search-button").on("click", function (event) {
         console.log(forecastArray.main.humidity);
     }
 
+    //Save user input city to local storage
+    localStorage.setItem("city", city);
+
 
 });
 
-    //Save city to local storage
-
-
-// Function to collect FUTURE date, temp, humidity related to city + related icon for 5 days
-
 //SEARCH HISTORY
-// Function to getItem(city) from local storage which search history is clicked
+// create a event listener to getItem(city) from local storage when matching search history city is clicked
+// $('#history').on('click', '#city', function(event){
+//     localStorage.getItem(event)
+// });
